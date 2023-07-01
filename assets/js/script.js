9cd70686bf893f4dd3fa7ce2606d47003fe77b9d
Ila1997 <124048687+Ila1997@users.noreply.github.com>
2023-07-01 12:25:41 +0200

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,3 @@
-
-// DISPLAY AND BUTTONS
-
-const counter = document.getElementById("counter-content");
-let plus = document.createElement("div");
-plus.classList = ["col-4 btn button inc"];
-counter.appendChild(plus);
-
-let display = document.createElement("div");
-display.classList = ["col-4 display-counter"];
-counter.appendChild(display);
-
-let minus = document.createElement("div");
-minus.classList = ["col-4 btn button dec"];
-counter.appendChild(minus);
-
-
-//ICONS
-
-const inc = document.querySelector(".inc");
-let iconPlus = document.createElement("i");
-iconPlus.classList = ["bi bi-plus-lg icon plus"];
-inc.appendChild(iconPlus);
-
-const dec = document.querySelector(".dec")
-let iconMinus = document.createElement("i");
-iconMinus.classList = ["bi bi-dash-lg icon minus"]
-dec.appendChild(iconMinus)
-
-
 //COUNT
 
 const counterText = document.querySelector(".display-counter");
